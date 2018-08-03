ac61465337167d00dae3bb239e40f44f90f4df16
Roystonpp <41322924+Roystonpp@users.noreply.github.com>
2018-08-03 11:24:29 +0100

    Add files via upload

--- src/TestingAccount/ServiceTest.java
+++ src/TestingAccount/ServiceTest.java
@@ -1,6 +1,8 @@
 package TestingAccount;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.util.HashMap;
 import org.junit.Test;
 import AccountApp.AccountManagement;
@@ -9,9 +11,9 @@ import AccountApp.Service;
 public class ServiceTest 
 {
 	HashMap hmap;
-	private AccountManagement account;
-	private Service service;
-	
+	public AccountManagement account;
+	public Service service;
+	 
 	@Test
 	public void hashTest() 
 	{
@@ -19,7 +21,27 @@ public class ServiceTest
 	service = new Service();
 	service.getAccount(1);
 	assertEquals("Did not find the account", service.getAccount(1));
+	}
 	
+	@Test
+	public void  jsonTest() 
+	{
+		Service s = new Service();
+		AccountManagement r = new AccountManagement("Royston", "Pinnock",10);
+		s.addAccount(r);
+		
+		HashMap<Integer, AccountManagement> hmap = new HashMap<Integer, AccountManagement>();
+		hmap.put(10, new AccountManagement("Royston", "Pinnock",10));
+        assertNotNull("Account is Null", s.MaptoJson());
+	}
+	
+	@Test
+	public void IterTest()
+	{
+		Service s = new Service();
+		AccountManagement r = new AccountManagement("Royston", "Pinnock",10);
+		s.addAccount(r);
+		assertEquals("This was not expected",s.Itername("Royston") , 1);
 	}
 
 }
